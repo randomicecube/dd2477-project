371447bfbe2b414085d2fc14cd14358e1d3183be
Diogo Gaspar <dgaspar@kth.se>
2024-05-05 23:01:31 +0200

    fix intersection and phrase searches

--- utils/search.py
+++ utils/search.py
@@ -14,19 +14,36 @@ def build_query(query_type, term):
         case "phrase":
             return search_phrase(term)
 
-# TODO: see if it makes sense to boost headline more than the content
-# TODO: check if it makes sense for any of them to be a should, instead of a must
-
 def search_intersection(terms):
     # Search for an intersection of multiple terms within an index
     query = QUERY_TEMPLATE
-    must_clauses = [{"multi_match": {"query": term, "fields": ["headline", "short_description"]}} for term in terms.split()]
-    query['query']['bool']['must'] = must_clauses
+    should_clauses = [
+        {
+            "bool": {
+                "should": [
+                    {"match": {"headline": term}},
+                    {"match": {"short_description": term}}
+                ],
+                "minimum_should_match": 1
+            }
+        }
+        for term in terms.split()
+    ]
+    query['query']['bool']['must'] = should_clauses
     return query
 
 def search_phrase(phrase):
     # Search for an exact phrase within an index
     query = QUERY_TEMPLATE
-    query['query']['bool']['must'].append({"match_phrase": {"headline": phrase}})
-    query['query']['bool']['must'].append({"match_phrase": {"short_description": phrase}})
+    query['query']['bool']['must'] = [
+        {
+            "bool": {
+                "should": [
+                    {"match_phrase": {"headline": phrase}},
+                    {"match_phrase": {"short_description": phrase}}
+                ],
+                "minimum_should_match": 1
+            }
+        }
+    ]
     return query
