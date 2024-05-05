6dec2696bde25ee4633a08fdbacaed16f11e0170
Diogo Gaspar <dgaspar@kth.se>
2024-05-05 23:01:00 +0200

    add dotenv to flake

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
       pythonPackages = p: with p; [
           numpy pandas scipy seaborn scikit-learn jupyter matplotlib pip
           django requests django-crispy-forms django-crispy-bootstrap4
-          elasticsearch tabulate elastic-transport
+          elasticsearch tabulate elastic-transport python-dotenv
         ];
     in {
       devShell.x86_64-linux = pkgs.mkShell {
